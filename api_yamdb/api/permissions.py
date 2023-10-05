974cc507baf298d937d6eb922f8c94f8fbe21a77
zaritskiyaa <zaritskiyaa@mail.ru>
2023-10-05 10:50:48 +0300

    fixed pip

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -42,5 +42,5 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-            request.user.is_admin 
+            request.user.is_admin
         )
